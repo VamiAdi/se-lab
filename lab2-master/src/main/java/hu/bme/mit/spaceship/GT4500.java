4936f5797901f2ac4728610f97a7eecfb3c1de23
VamiAdi <vamosadam03@gmail.com>
2023-09-27 17:00:46 +0200

    GT4500.java javitasa

--- lab2-master/src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ lab2-master/src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -80,6 +80,13 @@ public class GT4500 implements SpaceShip {
         // try to fire both of the torpedo stores
         //TODO implement feature
 
+        if(! primaryTorpedoStore.isEmpty() && !secondaryTorpedoStore.isEmpty()){
+          firingSuccess = primaryTorpedoStore.fire(1);
+          if(firingSuccess){
+            firingSuccess = secondaryTorpedoStore.fire(1);
+          }
+        }
+
         break;
     }
 
